801f73d9755e539d2d178498f07869cd91fa026d
Khan-Adeeb <adeebkhan2.786@gmail.com>
2025-12-31 23:32:23 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { WebSocketServer } from "ws";
+const wss = new WebSocketServer({ port: 8000 });
+
+wss.on("connection" ,function(socket){
+    console.log("userConnected");
+    setInterval(()=>{
+        socket.send("Current Price Of Gold : " + (Math.random()*1000))
+    }, 5000)
+})
